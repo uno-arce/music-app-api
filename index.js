1b552a96b46aa6f94fc8c7894695c76190f76d35
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-06 17:26:32 +0800

    Setup database, server, and middleware

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = required('dotenv')
+const userRoutes = require('./routes/userRoutes.js')
+
+// Database Connection
+dotenv.config()
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.connect(MONGODB_URI)
+mongoose.connection.once('Open', () => console.log('Now connected to MongoDB Atlas'))
+
+
+// Server Setup and Cors Configuration
+const app = express()
+
+const corsOptions = {
+	origin: ['http://localhost:3000'],
+	optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
+
+// Middleware
+app.use('/users', userRoutes)
+
+
+const PORT = process.env.PORT || 4000
+if(require.main === module) {
+	app.listen(PORT, () => console.log(`API is now online on port ${PORT]}`))
+}
