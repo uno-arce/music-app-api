a64dca3f7c757c628a435a7630057e6490cc55d8
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-07 16:16:49 +0800

    Setup JWT authentication functionalities

---
+++ auth.js
@@ -0,0 +1,52 @@
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+// Secret Keyword
+const secret = process.env.SECRET
+
+// Token Creation
+module.exports.createAccessToken = (user) => {
+	const data = {
+		id: user._id,
+		email: user.email,
+		isAdmin: user.isAdmin
+	}
+
+	return jwt.token(data, secret, {})
+} 
+
+// Token Verification
+module.exports.verify = (req, res, next) => {
+	const token = req.headers.authorization
+	if(typeof token === 'undefined') {
+		return res.status(400).send({ auth: 'Failed. No Token'} )
+	} else {
+		token = token.slice(7, token.length)
+	}
+
+	jwt.verify(token, secret, function(err, decodedToken) {
+		if(err) {
+			return res.status(403).send({
+				auth:  'Failed',
+				message: 'err.message'
+			})
+		} else {
+			req.user = decodedToken
+			next()
+		}
+	})
+}
+
+// Admin Verification
+module.exports.verifyAdmin = (req, res, next) => {
+	if(req.user.isAdmin) {
+		next()
+	} else {
+		return res.status(403).send({
+			auth: 'Failed',
+			message: 'Action Forbidden'
+		})
+	}
+}
