1a9eee22e7f20d2aa477a065393eb05a2ace7f4e
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-08 18:50:46 +0800

    Setup user registration route using express router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const { verify, verifyAdmin } = ('../auth')
+const userController = require('../controller/userController.js')
+
+router.post('/register', userController.registerUser)
 
 
 module.exports = router
