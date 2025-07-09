e1b9369fe9db67c7473325cfbb3e5f2e2c16860f
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-10 00:12:46 +0800

    Added the user registration function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { verify, verifyAdmin } = ('../auth')
-const userController = require('../controller/userController.js')
+const userController = require('../controllers/userController.js')
 
 router.post('/register', userController.registerUser)
 
