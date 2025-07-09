aa69d4e0eb8469a414fe409f05f15eea0405246b
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-10 00:14:47 +0800

    Added assertions for user registration success 200

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const chai = require('chai')
 const http = require('chai-http')
 const dotenv = require('dotenv')
+const expect = chai.expect
 chai.use(http)
 dotenv.config()
 
@@ -29,11 +30,19 @@ try {
 				.post('/users/register')
 				.type('json')
 				.send({
-					
+					 username: '333333',
+					 email: 'admin@gmail.com',
+					 password: 'admin123'
+				})
+				.end((err, res) => {
+					console.log(res.body.username)
+					expect(res).to.have.status(201)
+					expect(res.body.message).to.include('User registered successfully')
+					done(err)
 				})
 			})
 		})
-	}
+	})
 } catch(err) {
 	console.log('App cannot be found')
 	console.log(err)
