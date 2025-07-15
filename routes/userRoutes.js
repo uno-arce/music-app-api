2c9685f3d378ea9ba70f53517b1f4bd61ca5fb92
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-15 22:54:58 +0800

    Installed required dependencies for authorization and access to spotify

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const { verify, verifyAdmin } = ('../auth')
 const userController = require('../controllers/userController.js')
+const spotifyAuth = require('../spotifyAuth.js')
 
 router.post('/register', userController.registerUser)
 router.post('/login', userController.loginUser)
+router.get('/login', spotifyAuth)
 
 
 module.exports = router
