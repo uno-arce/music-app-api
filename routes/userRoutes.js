619be01216b97010c69533217ac98ef99b50da76
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-10 20:44:12 +0800

    Moved verify response to individual route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const { verify, verifyAdmin } = require('../auth')
 const userController = require('../controllers/userController.js')
-const spotifyAuth = require('../spotifyAuth.js')
 
 router.post('/register', userController.registerUser)
 router.post('/login', userController.loginUser)
 router.post('/logout', userController.logoutUser)
-router.get('/verify', verify)
+router.get('/verify', verify, (req, res) => {
+	res.status(200).send({ message: 'User is authenticated'})
+})
 router.post('/rate-songs', userController.addSongRatings)
 
 module.exports = router
