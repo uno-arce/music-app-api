4eee82e35808a13a339f73f824f767f65cd64523
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-03 16:51:06 +0800

    Modified cors options

--- index.js
+++ index.js
@@ -18,13 +18,14 @@ mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atl
 const app = express()
 
 const corsOptions = {
-	origin: [process.env.CLIENT_URL],
+	origin: process.env.CLIENT_URL,
 	credentials: true,
 	optionsSuccessStatus: 200
 }
 
-app.use(express.static(__dirname + '/public'))
 app.use(cors(corsOptions))
+app.options('*', cors(corsOptions))
+app.use(express.static(__dirname + '/public'))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -37,7 +38,7 @@ app.use('/auth/spotify', spotifyRoutes)
 
 const PORT = process.env.PORT || 4000
 if(require.main === module) {
-	app.listen(PORT, process.env.CLIENT_DOMAIN, () => console.log(`API is now online on port ${PORT}`))
+	app.listen(PORT, () => console.log(`API is now online on port ${PORT}`))
 }
 
 module.exports = {app, mongoose}
